a77b245d52d1b1eeb9e8b4976f79a375fa533ea5
nihalshanp <pnihalshan94@gmail.com>
2021-07-23 14:58:23 +0530

    created first reducer

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-const BUY_CAKE = "BUY_CAKE";
 
+
+// defining actions
+const BUY_CAKE = "BUY_CAKE";
 const buyCake = () => {
   return {
     type: BUY_CAKE,
     info: "First redux action",
   };
 };
+
+//initial State
+const initialState = {
+  numOfCakes: 10,
+};
+
+// (preveState, action) => newState
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
+
