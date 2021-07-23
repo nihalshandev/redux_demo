9b2162983aa7b4630f570c66e7bda5cabdb2146e
nihalshanp <pnihalshan94@gmail.com>
2021-07-23 15:07:56 +0530

    craeted first store and redux example

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-
+const  redux = require('redux')
+const createStore = redux.createStore
 
 // defining actions
 const BUY_CAKE = "BUY_CAKE";
@@ -27,3 +28,10 @@ const reducer = (state = initialState, action) => {
   }
 };
 
+const store = createStore(reducer)
+console.log('Initial state', store.getState())
+const unsubscribe = store.subscribe(()=> console.log('Updated state', store.getState()))
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+unsubscribe()
